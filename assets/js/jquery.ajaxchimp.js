6ff6d2ee8860c439d61f78893211b739b1162542
Steve Boyle <stevemboyle@Steves-MacBook-Air.local>
2016-11-14 17:34:43 -0800

    testing removing dataType

--- assets/js/jquery.ajaxchimp.js
+++ assets/js/jquery.ajaxchimp.js
@@ -121,7 +121,7 @@ For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsi
                     url: url,
                     data: data,
                     success: successCallback,
-                    dataType: 'jsonp',
+                    // dataType: 'jsonp',
                     error: function (resp, text) {
                         console.log('mailchimp ajax submit error: ' + text);
                     }
